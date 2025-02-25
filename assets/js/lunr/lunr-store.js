018b6c14ce4fc2e94b55cae5b268a207360d7b3e
Ben Thomasson <ben.thomasson@gmail.com>
2025-02-25 09:16:47 -0500

    new version Tue Feb 25 09:16:47 EST 2025

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "How does Ansible work?",
+        "excerpt":"Ansible is an automation tool that allows you to manage remote machines with modules. How do these modules work? Let’s take a look at the slack module as an example. First we need ansible installed on our local machine: python -m venv .venv source .venv/bin/activate pip install ansible Then download...","categories": ["ansible","code-review"],
+        "tags": [],
+        "url": "/ansible/code-review/how-does-ansible-work/",
+        "teaser": null
+      },{
+        "title": "Remote Ansible Modules",
+        "excerpt":"In this post I’ll answer the questions that arose from the last post: How do modules work on a remote host? Does the module need to run on the remote host or locally? How do we get the module to the remote host? Does python need to be installed there?...","categories": ["ansible","code-review"],
+        "tags": [],
+        "url": "/ansible/code-review/remote-ansible-modules/",
+        "teaser": null
+      },{
+        "title": "Remote Ansible Modules Continued",
+        "excerpt":"This post answers the question from the last post: How to modules import from dependencies? Do dependencies need to be installed on the remote host? Are dependencies shipped to the remote host? Most Ansible modules follow the pattern that the slack module does of importing utilties from ansible.module_utils. from ansible.module_utils.basic...","categories": ["ansible","code-review"],
+        "tags": [],
+        "url": "/ansible/code-review/remote-ansible-modules2/",
+        "teaser": null
+      },{
+        "title": "Smolagents are amazing",
+        "excerpt":"Smolagents is one of those projects that will change the industry. It was released at just the right time to build upon the recent open-source advances in reasoning models like deepseek-R1. The key insight here is that LLMs have been trained on a large corpus of Python code and can...","categories": ["smolagents","ai","agents"],
+        "tags": [],
+        "url": "/smolagents/ai/agents/smolagents-are-amazing/",
+        "teaser": null
+      }]
